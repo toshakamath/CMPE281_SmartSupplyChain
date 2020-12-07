eb5c01cf1b734f13cf79e4894bb331b5da981bfc
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-12-07 05:55:16 -0800

    return user details upon login to store in localstorage

--- tosha_backend/Controller/User.js
+++ tosha_backend/Controller/User.js
@@ -12,7 +12,8 @@ let login = (request, response) => {
         response.json({ status: "error", reason: "user not found" });
       } else {
         console.log(result[0]);
-        response.json({ status: "success", reason: "logged in successfully" });
+        delete result[0].password
+        response.json({ status: "success", reason: "logged in successfully", details:result[0] });
       }
     }
   });
